a96a60dc1ca78d2351538ee21be5f7092dde35d8
Pe6oProgramista <p.milev3@gmail.com>
2018-12-10 22:30:47 +0200

    last changes

--- 04.regex-css-selector-hw/input.js
+++ 04.regex-css-selector-hw/input.js
@@ -1,10 +1,10 @@
 regExps = {
 "exercise_1": /[A-Z][a-z]+/,
-"exercise_2": /088[1-9]\d{6,6}/,
+"exercise_2": /088[1-9]\d{6}/,
 "exercise_3": /[^10]+/,
-"exercise_4": /^((?!no).)*$/,
-"exercise_5": /^((\d{0,3})|(1[0-4]\d{0,2})|1500)$/,
-"exercise_6": /class=['"].*['"]/
+"exercise_4": /^[a-zA-Z][a-zA-Z_.0-9]{2,29}$/,
+"exercise_5": /^(\d{0,3}|1[0-4]\d{2}|1500)$/,
+"exercise_6": /class=('.*'|".*")/
 };
 cssSelectors = {
 "exercise_1": "item > java.class1",
@@ -13,6 +13,6 @@ cssSelectors = {
 "exercise_4": "#someId + item",
 "exercise_5": "tag > java + .class1",
 "exercise_6": "#someId item > item > item > item",
-"exercise_7": "different > different + different > java + java",
+"exercise_7": "#diffId2 > java + java",
 "exercise_8": "#someId"
 };
